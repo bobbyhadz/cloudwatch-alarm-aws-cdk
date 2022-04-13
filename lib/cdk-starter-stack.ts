1ae4b69b6ff52db017f5f16f21b8c5da7478bf96
Borislav Hadzhiev <opmftw@gmail.com>
2022-04-13 19:19:45 +0300

    update to cdk v2

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,6 +1,6 @@
-import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as cdk from '@aws-cdk/core';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cdk from 'aws-cdk-lib';
 import * as path from 'path';
 
 export class CdkStarterStack extends cdk.Stack {
